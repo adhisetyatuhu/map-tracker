f135aa185a0b6019ab313de4b9ed2c59ebaa9ea7
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2025-02-16 22:41:07 +0700

    Feat: responsive register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -21,8 +21,8 @@ function Register() {
 
     return (
         <div className="">
-            <div className="flex justify-between gap-5">
-                <div className="w-2/3 px-36">
+            <div className="flex justify-between sm:flex-wrap">
+                <div className="w-full sm:w-2/3 px-4 sm:px-20 md:px-36 lg:48 xl:px-60">
                     <div className="text-xl font-extrabold py-4">Logo</div>
 
                     <h1 className="text-2xl font-bold my-8 text-center">Create Your Account for Free</h1>
@@ -81,8 +81,8 @@ function Register() {
 
                         <button className="bg-blue-700 text-white font-bold py-2 mt-4 hover:bg-blue-700/90 hover:cursor-pointer active:bg-blue-700" type="submit">Create Account</button>
 
-                        <div class="h-3 my-4 border-b-2 border-gray-300 text-center">
-                            <span class="bg-white px-3">OR</span>
+                        <div className="h-3 my-4 border-b-2 border-gray-300 text-center">
+                            <span className="bg-white px-3">OR</span>
                         </div>
 
                         <button className="border border-gray-400 font-bold py-2 hover:bg-gray-100 hover:cursor-pointer active:bg-white" type="submit">Log in with Google</button>
@@ -90,21 +90,21 @@ function Register() {
                         <p className="text-center">Already have an account? <a className="text-blue-700" href="#">Log in</a></p>
                     </form>
                 </div>
-                <div className="w-1/3 bg-sky-800 flex justify-center min-h-screen px-4 py-30">
+                <div className="hidden sm:w-1/3 bg-sky-800 sm:flex sm:justify-center sm:min-h-screen px-4 py-30">
                     <div>
                         <h1 className="my-4 text-center text-xl text-white font-semibold">Trusted by the world's leading brands</h1>
-                        <div className="flex gap-2 flex-wrap px-2 justify-center">
-                            <figure className="w-20 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-28 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-20 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-20 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-36 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-36 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-30 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-20 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-36 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-36 h-8 bg-amber-50/30"></figure>
-                            <figure className="w-30 h-8 bg-amber-50/30"></figure>
+                        <div className="flex gap-3 flex-wrap px-2 justify-center">
+                            <figure className="mb-1 w-20 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-28 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-20 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-20 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-36 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-36 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-30 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-20 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-36 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-36 h-8 bg-amber-50/30"></figure>
+                            <figure className="mb-1 w-30 h-8 bg-amber-50/30"></figure>
                         </div>
                     </div>
                 </div>
