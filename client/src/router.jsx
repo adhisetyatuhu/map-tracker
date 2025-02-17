a5cab273c4d427c18996a66a9e9ea87611407943
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-02-17 22:41:08 +0700

    feat: SearchMapBox component

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Register from './pages/Register';
 import App from './App';
 import Login from './pages/Login';
+import SearchMapBox from './components/SearchMapBox';
 
 const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
         path: '/login',
         element: <Login />
     },
+    {
+        path: '/test',
+        element: <SearchMapBox/>
+    }
 ])
 
 export default router;
