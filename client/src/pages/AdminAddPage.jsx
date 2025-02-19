44653d9f9b1e9842362ba95bb15740b0165d4081
JeremyDezekiel <jeremydezekiel@gmail.com>
2025-02-19 23:57:08 +0700

    feat: add lucide-react dependency and update character set in AdminAddPage

--- client/src/pages/AdminAddPage.jsx
+++ client/src/pages/AdminAddPage.jsx
@@ -84,7 +84,7 @@ function AdminAddPage() {
     }, [])
 
     const generateRandomId = (length) => {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         let result = ''
         const charactersLength = characters.length
         for (let i = 0; i < length; i++) {
