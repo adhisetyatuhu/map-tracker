09325b0c10c4dcb08be1c472561587c4b857c03e
Adhi Setyatuhu <adhi.tuhu@gmail.com>
2025-02-21 05:32:09 +0700

    chore: icons for navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import Logout from "./Logout";
 import logo from "../assets/logo.png"
+import { HomeIcon } from "../utils/icons";
 
 export default function Header() {
     return (
@@ -10,7 +11,7 @@ export default function Header() {
                 <div className="flex items-center text-xl italic"><img className="h-12 bg-amber-50 rounded-full" src={logo} alt="" /></div>
                 <nav>
                     <ul className="flex items-center">
-                        <li><Link className="hover:bg-blue-700/10 hover:text-blue-700 px-4 py-2 rounded-md font-semibold" to="/">Home</Link></li>
+                        <li><Link className="hover:bg-blue-700/10 hover:text-blue-700 px-4 py-2 rounded-md font-semibold flex gap-1" to="/"><HomeIcon size={24} className="fill-blue-900" /> <span>Home</span></Link></li>
                         <li><Logout /></li>
                     </ul>
                 </nav>
